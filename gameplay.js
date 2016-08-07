7ac133c6f0691446b4f37ec2e851484b8de2726f
Qian <pandachain731@gmail.com>
2016-08-06 21:47:29 -0700

    basic functions completed

--- gameplay.js
+++ gameplay.js
@@ -2,12 +2,12 @@
  * Created by pandachain on 2016-08-05.
  */
 
-var Gameplay = function(number){
+var Gameplay = function(){
 
- var self = {number: number};
+ var self = {};
   self.board = ['', '', '', '', '', '', '', '', ''];
-  self.playerMark = 'X';
-  self.computerMark ='O';
+  self.playerMark = '';
+  self.computerMark ='';
 
   self.chooseAndAssignMark =  function(mark){
      self.playerMark = mark;
@@ -41,17 +41,14 @@ var Gameplay = function(number){
     }
   };
 
-  self.setComputerMove = function() {
-    var position = self.getComputerMove();
-    if (position !== 'No'){
-      self.board[position] = self.computerMark;
-    } else {
-      console.log('tie!')
+  self.setComputerMove = function(id) {
+    if (id !== 'No'){
+      self.board[id] = self.computerMark;
     }
   };
 
-  self.setPlayerMove = function(id, mark){
-    self.board[id] = mark;
+  self.setPlayerMove = function(id){
+    self.board[id] = self.playerMark;
   };
 
 
@@ -63,9 +60,7 @@ var Gameplay = function(number){
       var copyBoard = self.copyCurrentBoard();
       if (self.isSpaceFree(copyBoard, i)) {
         copyBoard[i] = self.computerMark;
-        console.log("copyboard: " + i + ": " + copyBoard);
         if (self.isWining(copyBoard)) {
-          console.log(1)
           return i
         }
       }
@@ -78,7 +73,6 @@ var Gameplay = function(number){
       if (self.isSpaceFree(copyBoard2, j)) {
         copyBoard2[j] = self.playerMark;
         if (self.isWining(copyBoard2)) {
-          console.log(2)
           return j
         }
       }
@@ -87,39 +81,18 @@ var Gameplay = function(number){
     //3. put on one of the corners, if free
     var position = self.chooseRandomPosition([0, 2, 6, 8]);
     if (position !== 'No') {
-      console.log(3)
       return position
     }
 
     //4. put on center, if free
     if (self.isSpaceFree(self.board, 4)) {
-      console.log(4)
       return 4;
     }
 
     //5. put on one of the sides, if free
-    console.log(5)
     return self.chooseRandomPosition([1, 3, 5, 7]);
 
     };
-    
-    // if (self.checkComputerWinNext(copyBoard) !== 'No') {
-    //   console.log('1');
-    //   console.log(self.checkComputerWinNext(copyBoard));
-    //   // self.board[self.checkComputerWinNext(copyBoard)] = self.computerMark;
-    // } else if(self.checkPlayerWinNext(copyBoard) !== 'No') {
-    //   console.log('2')
-    //   self.board[self.checkPlayerWinNext(copyBoard)] = self.computerMark;
-    // } else if (self.takeOneCorner() !== 'No') {
-    //   console.log('3')
-    //   self.board[self.takeOneCorner()] = self.computerMark;
-    // } else if (takeCenter() !== 'No') {
-    //   console.log('4')
-    //   self.board[self.takeCenter()] = self.computerMark;
-    // } else {
-    //   console.log('5')
-    //   self.board[self.takeOneSide()] = self.computerMark;
-    // }
 
   self.isSpaceFree = function(board, id){
     return board[id] === '';
@@ -149,11 +122,11 @@ var Gameplay = function(number){
     return (self.board.indexOf('') === -1)
   };
 
+  self.reset  = function(){
+    self.board = ['', '', '', '', '', '', '', '', ''];
+  };
+
   return self;
 
 };
-
-var game1 = Gameplay(1);
-game1.setComputerMove();
-console.log(game1.board)
 
--- main.js
+++ main.js
@@ -1,3 +1,82 @@
 /**
  * Created by pandachain on 2016-08-06.
  */
+
+// this file uses scripts from gameplay.js
+var currentPlayerMark = '';
+var currentGameBoard = [
+  document.querySelector('#topLeft'),
+  document.querySelector('#topMiddle'),
+  document.querySelector('#topRight'),
+  document.querySelector('#left'),
+  document.querySelector('#middle'),
+  document.querySelector('#right'),
+  document.querySelector('#bottomLeft'),
+  document.querySelector('#bottomMiddle'),
+  document.querySelector('#bottomRight')
+];
+
+var gameInfoArea = document.querySelector('#gameControl');
+
+var startGame = function(){
+  if (document.querySelector('#X').checked === true) {
+    currentPlayerMark = document.querySelector('#X').value;
+    gameInfoArea.innerHTML = '<p>You start!';
+  } else if(document.querySelector('#O').checked === true) {
+    currentPlayerMark = document.querySelector('#O').value;
+    gameInfoArea.innerHTML = '<p>Computer start!';
+  }
+  var newGame = Gameplay();
+  newGame.chooseAndAssignMark(currentPlayerMark);
+  play(newGame);
+};
+
+
+var play = function(gameplay){
+  if (gameplay.playerMark === 'O'){
+    var firstMove = gameplay.getComputerMove();
+    currentGameBoard[firstMove].textContent = gameplay.computerMark;
+    gameplay.setComputerMove(firstMove);
+  }
+  currentGameBoard.forEach(function (boardPosition) {
+    boardPosition.addEventListener('click', function () {
+      this.textContent = gameplay.playerMark;
+      var playerMove = currentGameBoard.indexOf(this);
+      gameplay.setPlayerMove(playerMove);
+      if (gameplay.isWining(gameplay.board)) {
+        gameInfoArea.innerHTML = '<p>You won!';
+        startAgain(gameplay);
+      } else if (gameplay.isBoardFull()) {
+        gameInfoArea.innerHTML = "<p>It's a tie!";
+        startAgain(gameplay);
+      } else {
+        var computerMove = gameplay.getComputerMove();
+        currentGameBoard[computerMove].textContent = gameplay.computerMark;
+        gameplay.setComputerMove(computerMove);
+        if (gameplay.isWining(gameplay.board)) {
+          gameInfoArea.innerHTML = 'You lost!';
+          startAgain(gameplay);
+        } else if(gameplay.isBoardFull()) {
+          gameInfoArea.innerHTML = "<p>It's a tie!";
+          startAgain(gameplay);
+        }
+      }
+    })
+  })
+};
+
+var startAgain = function(gameplay){
+  clearBoard();
+  gameplay.reset();
+};
+
+
+var clearBoard = function(){
+  currentGameBoard.forEach(function(boardPosition){
+    boardPosition.textContent = '';
+  })
+};
+
+var startButton = document.querySelector('#start');
+startButton.onclick = startGame;
+
